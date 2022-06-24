ce8841c0ecef607f744beadb8c039e2cb7f28caf
idian1 <108105770+idian1@users.noreply.github.com>
2022-06-24 00:03:40 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+suma = 0
+num = int(input("ingrese un numero "))
+for i in range(0, num+1):
+    if i % 2 == 1:
+        suma += i
+
+print(f"la suma de todos los numeros impares entre 0 y {num} es de {suma}")
+input("")
+# En este codigo el usuario ingresa un numero el que entra en un ciclo "for" el cual recorre todos los numeros desde 0
+# hasta el numero ingresado (num) y utilizando el operador modulo comprueba si el numero es par o impar, si este es
+# impar sera sumado a la variable suma, luego de terminar el ciclo imprime la suma total de impares
+# En este codigo utilice el ciclo for por que era conveniente para la situacion requerida.
